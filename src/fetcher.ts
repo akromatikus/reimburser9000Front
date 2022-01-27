6702e658955068ce3dc6af062838a50178d632d9
akromatikus <akromatikus.contact@gmail.com>
2022-01-27 15:39:05 -0600

    azurecomplete

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -6,22 +6,25 @@ export default async function fetcher(inputs, fetchtype:string){
         case 'check-if-user':
             const {...input} = inputs
             backendRes = 
-            await fetch('http://localhost:5000/check-if-user', {
+            await fetch('https://reimburser9000-backend.azurewebsites.net/check-if-user', {
                     method:'PATCH', body: JSON.stringify(input), 
                     headers:{'Content-Type':'application/json'}
             })
+            //'http://localhost:5000/check-if-user'
             return (await backendRes.json())
         case 'get-userlist':
             backendRes =
-            await fetch('http://localhost:5000/userlist')
+            await fetch('https://reimburser9000-backend.azurewebsites.net/userlist')
+            //'http://localhost:5000/userlist'
             return (await backendRes.json())
         case 'update-users':
             const usersToUpdate: user[] = inputs
             backendRes =
-            await fetch('http://localhost:5000/update-users', {
+            await fetch('https://reimburser9000-backend.azurewebsites.net/update-users', {
                 method:'PATCH', body: JSON.stringify(usersToUpdate), 
                 headers:{'Content-Type':'application/json'}
             })
+            //'http://localhost:5000/update-users'
             break;
         default:
             return (Error("no fetch of that type exists"))       
